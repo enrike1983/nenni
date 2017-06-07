65728f7b936158d530d2c92b934ad97871691d19
Enrico Antonello <eantonel@gmail.com>
2017-06-07 17:34:18 +0200

    updates per backend

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -15,6 +15,9 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle(),
+            new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
+
             new AppBundle\AppBundle(),
         ];
 
